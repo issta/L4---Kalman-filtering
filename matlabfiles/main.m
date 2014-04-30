2d14d1d5b695d30e46bc00778e4b98b725132938
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-30 14:45:53 +0200

    Imported true and measured values

--- matlabfiles/main.m
+++ matlabfiles/main.m
@@ -2,15 +2,25 @@
 clear all; clc;
 addpath(['/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L4 ',...
 '- Kalman filtering/']);
-data = xlsread('Measurement.xlsx');
-time = data(1:25,1);
-east = data(1:25,2);
-north = data(1:25,3);
-speed = data(1:25,4);
+% Import measured values from sheet 1
+data_meas = xlsread('Measurement.xlsx');
+time_meas = data_meas(1:25,1);
+east_meas = data_meas(1:25,2);
+north_meas = data_meas(1:25,3);
+speed_meas = data_meas(1:25,4);
+% Import true values from sheet 2
+data_true = xlsread('Measurement.xlsx','True values');
+time_true = data_true(1:25,1);
+east_true = data_true(1:25,2);
+north_true = data_true(1:25,3);
+vel_east_true = data_true(1:25,4);
+vel_north_true = data_true(1:25,5);
 %% A priori statistics
-psd = 0.1; % - PSD (power spectral density) of the random acceleration:
-semc = 3; % - Standard error of measured coordinates:
-semav = 0.5;% - Standard error of measured abs. velocity:
-seiv = 0.01;% - Standard error of initial velocity:
-seic = 0.1;% - Standard error of initial coordinates:
+PSD = 0.01; % - PSD (power spectral density) of the random acceleration
+sd_meas_coord = 3; % m - Standard error of measured coordinates
+sd_meas_abs_vel = 0.5; % m/s - Standard error of measured abs. velocity
+sd_ini_vel = 3; % m/s - Standard error of initial velocity
+sd_ini_coord = 10; % m - Standard error of initial coordinates
+vel_east = 3.53; % m/s
+vel_north = 0.86; % m/s
 %% 
