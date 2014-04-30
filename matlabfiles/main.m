2277985f6a80fd8a60d8f5868a5ef0bd14e37b1a
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-30 14:35:40 +0200

    saved at school with new data online

--- matlabfiles/main.m
+++ matlabfiles/main.m
@@ -1,9 +1,16 @@
 %% Kalman Filter
 clear all; clc;
+addpath(['/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L4 ',...
+'- Kalman filtering/']);
+data = xlsread('Measurement.xlsx');
+time = data(1:25,1);
+east = data(1:25,2);
+north = data(1:25,3);
+speed = data(1:25,4);
 %% A priori statistics
 psd = 0.1; % - PSD (power spectral density) of the random acceleration:
 semc = 3; % - Standard error of measured coordinates:
 semav = 0.5;% - Standard error of measured abs. velocity:
 seiv = 0.01;% - Standard error of initial velocity:
 seic = 0.1;% - Standard error of initial coordinates:
-% Measured coordinates:
+%% 
