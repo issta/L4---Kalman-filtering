7d708b78621614e96c84d79f5dd21f4fa9492e0f
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-02 00:20:16 +0200

    for loop with bad results

--- matlabfiles/main.m
+++ matlabfiles/main.m
@@ -1,6 +1,7 @@
 %% Kalman Filter
-clear all;
-addpath(['/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L4 ',...
+clear all;clc;close all
+addpath(['/Users/kevin/SkyDrive/KTH Work/',...
+    'Period 4 2014/GNSS/Labs/L4 ',...
     '- Kalman filtering/']);
 % Import measured values from sheet 1
 data_meas = xlsread('Measurement.xlsx');
@@ -16,9 +17,11 @@ north_true = data_true(1:25,3);
 vel_east_true = data_true(1:25,4);
 vel_north_true = data_true(1:25,5);
 %% A priori statistics
-PSD = 0.01; % - PSD (power spectral density) of the random acceleration
+PSD = 0.01; % - PSD (power spectral density) of the
+% random acceleration
 sd_meas_coord = 3; % m - Standard error of measured coordinates
-sd_meas_abs_vel = 0.5; % m/s - Standard error of measured abs. velocity
+sd_meas_abs_vel = 0.5; % m/s - Standard error of measured
+% abs. velocity
 sd_ini_vel = 3; % m/s - Standard error of initial velocity
 sd_ini_coord = 10; % m - Standard error of initial coordinates
 ve = 3.53; % m/s
@@ -26,7 +29,7 @@ vn = 0.86; % m/s
 dt = 2; % time difference -> 2 sec between measurements
 %% State variables
 xk = [east_meas(1) north_meas(1) ve vn]';
-xkoriginal = xk;
+
 % Equation 4
 F = zeros(4);
 F(1,3) = 1;
@@ -55,7 +58,7 @@ Hk = [1,0, 0,        0;...
     0, 0, ve/vm,  vn/vm];
 Lk = Hk * xk;
 Rk = cov(Lk);
-for i = 1:25
+for i=1:25
     %% Equation 16
     % Time propagation
     xk(:,i+1) = Tk * xk(:,i);
@@ -63,21 +66,29 @@ for i = 1:25
     %% Equation 17
     % Gain calculation
     Kk = Qx * Hk'/(Rk + Hk * Qx * Hk');
-	%% Equation 18
-	% Measurement update
-	xk(:,i+1) = xk(:,i) + Kk*[Lk-Hk*xk(:,i)];
+    %% Equation 18
+    % Measurement update
+    xk(:,i+1) = xk(:,i) + Kk*[Lk-Hk*xk(:,i)];
     %% Equation 19
     Qx = [eye(length(Kk*Hk))-Kk*Hk]*Qx;
     %% Equation 22
-%     Lk = [east_meas(i) north_meas(i) sqrt(ve^2 + vn^2)]';
+    Lk = [east_meas(i) north_meas(i) sqrt(ve^2 + vn^2)]';
     xplot(:,i+1) = xk(:,i);
 end
-clf;
-x1 = xplot(1,:); % final values
-y1 = xplot(2,:); % final values
-x2 = xkoriginal(1,:);
-y2 = xkoriginal(2,:);
-plot(x1,y1,x2,y2)
-% hold on
-% plot(east_meas,north_meas,'r')
+x1 = xk(1,:); % final values
+y1 = xk(2,:); % final values
+x2 = east_meas;
+y2 = north_meas;
+x3 = east_true;
+y3 = north_true;
+finalplot = plot(x1,y1,'.','color','r');
+hold on;
+originalplot = plot(x2,y2,'-.','color','r');
+trueplot = plot(x3,y3,'.');
+legend([finalplot,originalplot,trueplot],...
+    'Final','Original','True',...
+    'location','best');
+hold off;
+
+% hold on plot(east_meas,north_meas,'r')
 % plot(east_true,north_true,'b')
