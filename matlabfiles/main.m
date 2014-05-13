2397978fac086e97de482cd075f452ff6469ed06
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-13 15:36:10 +0200

    more corrections but no closer to his answer
    maybe in the smoothing, check to see if answers are the same before
    smoothing

--- matlabfiles/main.m
+++ matlabfiles/main.m
@@ -50,7 +50,7 @@ QG = G*Q*G';
 Qk = QG * dt + (F*QG + QG*F')*dt^2/2 + F*QG*F'*dt^3/3;
 %% Equation 15
 Qx(:,:,1) = diag([sd_ini_coord^2 ...    % covariance matrix of initial state
-   sd_ini_coord^2 sd_ini_vel^2 sd_ini_vel^2]);
+    sd_ini_coord^2 sd_ini_vel^2 sd_ini_vel^2]);
 Rk = diag([meas.sd_coord meas.sd_coord meas.sd_abs_vel]);
 Qxm_predicted = zeros(4,4,25);
 %% FOR LOOP
@@ -58,28 +58,22 @@ for i=1:25
     %% Equation 25
     %% Equation 26
     % Lk = Hk * xk;
-    Lk = [meas.east(i) meas.north(i)...
-        meas.speed(i)]';
-%     if i == 1
-%         Lktilde = Lk + [sd_ini_coord, sd_ini_coord, sd_ini_vel]';
-%     else
-%         Lktilde = Lk + [meas.sd_coord, meas.sd_coord, meas.sd_abs_vel]';
-%     end
-    
-    %
-    xkm_predicted(:,i+1) = Tk * xk(:,i); % Equation 16 Time propagation
-%     Qx = cov(xk(:,i));
+    xkm_predicted(:,i) = Tk * xk(:,i); % Equation 16 Time propagation
+    %     Qx = cov(xk(:,i));
     Qxm_predicted(:,:,i) = Tk * Qx(:,:,i) * Tk' + Qk; % Equation 16
-    vm_predicted = sqrt(xkm_predicted(3,i+1)^2 + xkm_predicted(4,i+1)^2); % should be equal to speed_meas(1)
-    Hk = [1,0, 0,        0;...
+    vm_predicted = sqrt(xkm_predicted(3,i)^2 + xkm_predicted(4,i)^2); % should be equal to speed_meas(1)
+    Hk(:,:,i) = [1,0, 0,        0;...
         0, 1, 0,        0;...
-        0, 0, xkm_predicted(3,i+1)/vm_predicted,  xkm_predicted(4,i+1)/vm_predicted];
-    hkm_predicted = [xkm_predicted(1,i+1) xkm_predicted(2,i+1) sqrt(xkm_predicted(3,i+1)^2 + xkm_predicted(4,i+1)^2)]';
-    Kk = Qxm_predicted(:,:,i) * Hk'*inv([Rk + Hk * Qxm_predicted(:,:,i) * Hk']); % Equation 17 Gain
-    xk(:,i+1) = xkm_predicted(:,i+1) + Kk*[ Lk - hkm_predicted ]; % Equation18
+        0, 0, xkm_predicted(3,i)/vm_predicted,  xkm_predicted(4,i)/vm_predicted];
+    Kk(:,:,i) = Qxm_predicted(:,:,i) * Hk(:,:,i)'*inv([Rk + Hk(:,:,i) * Qxm_predicted(:,:,i) * Hk(:,:,i)']); % Equation 17 Gain
+    Lk(:,i) = [meas.east(i) meas.north(i)...
+        meas.speed(i)]';
+    hkm_predicted = [xkm_predicted(1,i) xkm_predicted(2,i) sqrt(xkm_predicted(3,i)^2 + xkm_predicted(4,i)^2)]';
+    
+    xk(:,i+1) = xkm_predicted(:,i) + Kk(:,:,i)*[ Lk(:,i) - hkm_predicted ]; % Equation18
     %     Measurement update
     % Equation 19
-    Qx(:,:,i+1) = [eye(length(Kk*Hk))-Kk*Hk]*Qxm_predicted(:,:,i);
+    Qx(:,:,i+1) = [eye(length(Kk(:,:,i)*Hk(:,:,i)))-Kk(:,:,i)*Hk(:,:,i)]*Qxm_predicted(:,:,i);
     % Equation 22
     %     Lk = [meas.east(i) meas.north(i)...
     %         sqrt((xk(3,i))^2 + (xk(4,i))^2)]';
@@ -94,7 +88,7 @@ count = nStep;
 Qxkhat(:,:,25) = Qx(:,:,end);
 for i = 1:(nStep-1)
     Dk = Qx(:,:,count+1)*Tk'*inv(Qxm_predicted(:,:,count));
-    xkhat(:,count-1) = xk(:,count) + Dk*[xkhat(:,count) - xkm_predicted(:,count+1)];
+    xkhat(:,count-1) = xk(:,count) + Dk*[xkhat(:,count) - xkm_predicted(:,count)];
     Qxkhat(:,:,count-1) = Qx(:,:,count+1) + Dk*[Qxkhat(:,:,count) - Qxm_predicted(:,:,count)]*Dk';
     count = count - 1;
 end
@@ -106,17 +100,24 @@ meas.x2 = meas.east; % original
 meas.y2 = meas.north; % original
 true.x3 = true.east; % true
 true.y3 = true.north; % true
-final.plot = plot(final.x1,final.y1,'b');
+% final.plot = plot(final.x1,final.y1,'b');
 hold on;
 xk_smooth = plot(xkhat(1,:),xkhat(2,:),'c');
 % originalplot = plot(meas.x2,meas.y2,'r');
 % hisplot = plot(e_p,n_p,'g');
 hisSmoothing = plot(x_s(1,:),x_s(2,:),'m');
 true.plot = plot(true.x3,true.y3,'-','color','k');
+legend([hisSmoothing,...
+    xk_smooth,...
+    true.plot],...
+    'His Smoothing',...
+    'My Smoothing',...
+    'True Plot',...
+    'location','best');
 % legend([final.plot,originalplot,true.plot,hisplot,hisSmoothing,xk_smooth],...
 %     'Final','Original','True','His predictions','His Smoothing',...
 %     'My Smoothing','location','best');
-% hold off;
-
+hold off;
+difference = sum(sqrt((xkhat(1,:) - x_s(1,:)).^2))/length(xkhat)
 % hold on plot(east_meas,north_meas,'r')
 % plot(east_true,north_true,'b')
